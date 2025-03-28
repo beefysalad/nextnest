000cf8e599b5d18e5f04f86f545b9219f0cc9e5b
John Patrick Ryan Mandal <85030330+beefysalad@users.noreply.github.com>
2025-03-29 00:51:59 +0800

    Update page.tsx

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -50,7 +50,7 @@ export default async function Home() {
             target='_blank'
             rel='noopener noreferrer'
           >
-            UPDATED FRONTEND TEST
+            TANGNA
           </a>
         </div>
       </main>
